c2f15ae4e4ae3a0d32f7df0a6e557a8c205d851b
Christoffer Olsen <chris70944@gmail.com>
2024-02-20 11:17:25 +0100

    Færdig :)

--- Opgave 3.5/Program.cs
+++ Opgave 3.5/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Skriv dit første tal her:");
 int tal = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Dit første tal var {tal}");
+
+Console.WriteLine("Skriv dit andet tal her");
+tal = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Dit andet tal var {tal}");
